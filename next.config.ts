80b975c3d3d22159dd2fe74f481434d11be60b40
CainXEC <your-github-email@example.com>
2026-04-11 12:15:30 -0700

    redirect xec-publish.vercel.app to proofofwriting.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'xec-publish.vercel.app',
+          },
+        ],
+        destination: 'https://www.proofofwriting.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
